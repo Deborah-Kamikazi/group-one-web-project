c1cece4b60a53ec69676421bf16d475a77cd5285
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-05-31 12:19:11 +0200

    fix: Matched custom colors with Tailwind Color naming conventions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        webGreen: "#00672E",
-        webGrey: "#EFECEA"
-      }
+        "webgreen-100": "#00672E",
+        "webgray-100": "#EFECEA",
+      },
+      flexGrow: {
+        1.2: 1.2,
+        2: 2,
+      },
     },
   },
   plugins: [],
-}
+};
